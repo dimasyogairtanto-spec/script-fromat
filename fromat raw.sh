43043f06b6bfdb7fe9b0d9be8fdbba48778011df
FreeHorse <dimasyogairtanto@gmail.com>
2026-01-22 16:48:34 +0700

    Update fromat raw.sh

--- fromat raw.sh
+++ fromat raw.sh
@@ -1,6 +1,7 @@
 umount -R /mnt &&
 mkfs.ext4 -b 4096 /dev/sda1 &&
 mount /dev/sda1 /mnt &&
+mkdir /mnt/boot &&
 mkfs.vfat -F32 -S 4096 -n BOOT /dev/sda2 &&
 mount -o uid=0,gid=0,fmask=0077,dmask=0077 /dev/sda2 /mnt/boot &&
 mkdir /mnt/home &&
