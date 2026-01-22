8ba7515128348921a4348ce18cc544f03afb1110
FreeHorse <dimasyogairtanto@gmail.com>
2026-01-22 17:17:50 +0700

    Update fromaat.sh

--- fromaat.sh
+++ fromaat.sh
@@ -4,15 +4,15 @@ BOOT_PART="/dev/sda2"
 DATA_PART="/dev/sda3"
 
 umount -R /mnt &&
-echo "==> Format root"
-mkfs.ext4 -b 4096 ${ROOT_PART} && 
-mount $ROOT_PART /mnt &&
-echo "==> Format boot"
-mkdir /mnt/boot &&
-mkfs.vfat -F32 -S 4096 -n BOOT ${BOOT_PART} &&
-mount $BOOT_PART /mnt/boot &&
-echo "==> Format data"
-mkdir /mnt/home &&
-mkfs.ext4 -b 4096 ${BOOT_PART} &&
-mount $DATA_PART /mnt/home &&
-genfstab -U /mnt >> /mnt/etc/fstab
+#echo "==> Format root"
+#mkfs.ext4 -b 4096 ${ROOT_PART} && 
+#mount $ROOT_PART /mnt &&
+#echo "==> Format boot"
+#mkdir /mnt/boot &&
+#mkfs.vfat -F32 -S 4096 -n BOOT ${BOOT_PART} &&
+#mount $BOOT_PART /mnt/boot &&
+#echo "==> Format data"
+#mkdir /mnt/home &&
+#mkfs.ext4 -b 4096 ${BOOT_PART} &&
+#mount $DATA_PART /mnt/home &&
+#genfstab -U /mnt >> /mnt/etc/fstab
