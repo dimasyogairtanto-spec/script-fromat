e7147f238afe755402670bd904459f4b98f546cb
FreeHorse <dimasyogairtanto@gmail.com>
2026-01-22 16:55:15 +0700

    Add mkdir commands for boot and home directories
    Create directories for boot and home partitions before formatting.

--- fromaat.sh
+++ fromaat.sh
@@ -8,9 +8,11 @@ echo "==> Format root"
 mkfs.ext4 -b 4096 ${ROOT_PART} && 
 mount $ROOT_PART /mnt &&
 echo "==> Format boot"
+mkdir /mnt/boot &&
 mkfs.vfat -F32 -S 4096 -n BOOT ${BOOT_PART} &&
 mount $BOOT_PART /mnt/boot &&
 echo "==> Format data"
+mkdir /mnt/home &&
 mkfs.ext4 -b 4096 ${BOOT_PART} &&
 mount $DATA_PART /mnt/home &&
 genfstab -U /mnt >> /mnt/etc/fstab
