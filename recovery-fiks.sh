354126a38ec8f0f9c7fa0bcf57b50dabe1c528e5
FreeHorse <dimasyogairtanto@gmail.com>
2026-01-23 21:05:42 +0700

    Update recovery-fiks.sh

--- recovery-fiks.sh
+++ recovery-fiks.sh
@@ -7,7 +7,7 @@ mkfs.vfat -F32 -S 4096 -n BOOT ${boot} &&
 mount -o uid=0,gid=0,fmask=0077,dmask=0077 "$boot" /mnt &&
 mount "$fd" /opt &&
 mkdir /mnt/{efi,loader,kernel} &&
-mkdir /mnt/efi{linux,boot,recovery,systemd} &&
+mkdir /mnt/efi/{linux,boot,recovery,systemd} &&
 bootctl --path=/mnt install &&
 cp /opt/arch/boot/x86_64/* /mnt/efi/recovery &&
 cp -r /opt/arch/x86_64 /mnt/efi/recovery &&
@@ -17,5 +17,6 @@ sed <<EOF
      linux      /efi/recovery/vmlinuz-linux
      initrd     /efi/recovery/initramfs-linux.img
      options    archisobasedir=efi/recovery archisolabel=BOOT copytoram' /mnt/loader/entries/recovery.conf
+     
      EOF
 
