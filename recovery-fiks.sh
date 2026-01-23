1894ff8db29ed894b75ecac3d60b233c828d744a
FreeHorse <dimasyogairtanto@gmail.com>
2026-01-23 13:33:41 +0700

    Add recovery-fiks.sh for boot partition setup
    This script formats a boot partition, mounts it, and sets up the necessary directories and bootloader for recovery.

---
+++ recovery-fiks.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+read -p "partisi boot: " boot
+read -p "partisi fd: " fd
+
+echo "==> Format boot"
+mkfs.vfat -F32 -S 4096 -n BOOT ${boot} &&
+mount -o uid=0,gid=0,fmask=0077,dmask=0077 "$boot" /mnt &&
+mount "$fd" /dev/opt &&
+mkdir /mnt/{efi,loader,kernel} &&
+mkdir /mnt/efi{linux,boot,recovery,systemd} &&
+bootctl --path=/mnt install &&
+cp /opt/arch/boot/x86_64/* /mnt/efi/recovery &&
+cp -r /opt/arch/x86_64 /mnt/efi/recovery &&
+sed 'title      recovery
+     versions   archiso
+     linux      /efi/recovery/vmlinuz-linux
+     initrd     /efi/recovery/initramfs-linux.img
+     options    archisobasedir=efi/recovery archisolabel=BOOT copytoram' /mnt/loader/entries/recovery.conf
+
